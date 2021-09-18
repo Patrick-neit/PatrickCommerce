47da9a384ac2797e95d25430bb1553768955b9c9
Patrick-neit <patrickaguilar2403@gmail.com>
2021-09-18 00:28:32 -0500

    Diseño header e iconos

---
+++ resources/views/livewire/dropdown-cart.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <x-jet-dropdown>
+        <x-slot name="trigger">
+            <x-cart color="white" />
+        </x-slot>
+
+        <x-slot name="content">
+
+        </x-slot>
+
+    </x-jet-dropdown>
+</div>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,4 +1,12 @@
-<div class="flex-1">
+<div class="flex-1 relative">
     <x-jet-input type="input items-center px-4" class="w-full" placeholder="Looking for something?" />
+
+
+
+    <!--Llamamos al componente creado en components/search.blade-->
+    <button class="absolute top-0 right-0 w-12 h-full bg-red-500 flex items-center justify-center rounded-r-md">
+        <x-search size="25" color="white" />
+    </button>
+
 
 </div>
